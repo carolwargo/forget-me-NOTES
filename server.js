0c009343578def3781ad9d325a8231c6560b78c8
Carol Wargo <Carolwargo@gmail.com>
2023-06-04 16:43:16 -0400

    update pathing notes.html

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, './public/404.html'))
 );
 
 app.listen(PORT, () =>
